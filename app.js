e425acad5212755ee9bd68ffcfc136edb799c82e
Maria Efremova <maria375797@gmail.com>
2020-10-04 23:30:35 +0300

    Add fns-date and create a hash function

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ const fileName = 'input.json';
 const data = jsonData(`./${fileName}`);
 console.log(data);
 
-const result = data.map(transaction => {
-  switch(transaction.type) {
-    case 'cash_in':
-      return cashIn(transaction);
-    case 'cash_out':
-      return cashOut(transaction);
-    default:
-      return null;
-  }
-});
+const result = data
+  .map(transaction => {
+    switch(transaction.type) {
+      case 'cash_in':
+        return cashIn(transaction);
+      case 'cash_out':
+        return cashOut(transaction, data);
+      default:
+        return null;
+    }
+  });
 
 console.log(...result);
 
--- calculations/cashOut.js
+++ calculations/cashOut.js
@@ -1,4 +1,16 @@
 const rounding = require('./rounding');
+const { getISOWeek, getYear } = require('date-fns');
+
+function calculateWeekHash(data) {
+  return data.map(transaction => {
+    const date = new Date(transaction.date)
+    return {
+      isoWeek: getISOWeek(date),
+      year: getYear(date),
+      userId: transaction.user_id,
+    }
+  })
+}
 
 function cashOutJuridical(transaction) {
   const fullFee = transaction.operation.amount * 0.3 / 100;
@@ -10,12 +22,13 @@ function cashOutNatural(transaction) {
   return rounding((fullFee > 0.5) ? fullFee : 0.50);
 }
 
-function cashOut(transaction) {
+function cashOut(transaction, data) {
+  console.log(calculateWeekHash(data))
   switch(transaction.user_type) {
     case 'juridical':
       return cashOutJuridical(transaction);
     case 'natural':
-      return cashOutNatural(transaction);
+      return cashOutNatural(transaction, data);
     default:
       return null;
   }
